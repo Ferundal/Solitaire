667f3eadad86f100253708a402db7ea17d99a2bb
ferundal <ferundal@gmial.com>
2022-07-25 21:08:38 +0400

    MVC core
    Some interfaces for MVC pattern and SOLID

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private ITableModel tableModel;
+    private void Awake()
+    {
+        tableModel = new TableModel();
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
